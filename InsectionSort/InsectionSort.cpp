1ec69b2d665525a4a4c594d017ad299eb46c1ac5
galang48 <galangyudha1945@gmail.com>
2024-03-14 10:29:49 +0700

    membuat inputan jumlah element array

--- InsectionSort/InsectionSort.cpp
+++ InsectionSort/InsectionSort.cpp
@@ -4,7 +4,21 @@ using namespace std;
 int arr[20];                                            // Membuat Array dengan panjang data 20
 int n;                                                  // Membuat Variable inputan n
 
+void input() {                       // Procedure Input
+    while (true)
+    {
+        cout << "Masukkan Jumlah Data pada Array : ";   // Membuat Inputan jumlah element Array
+        cin >> n;                                       // memanggil variable inputan n
 
+        if (n <= 20) {                                  // Membuat Kondisi n tidak lebih dari 20
+            break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 elemen.\n";      // Menampilkan pesan jika data lbih dari 20
+        }
+    }
+}
 int main()
 {
     std::cout << "Hello World!\n";
