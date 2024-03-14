1fa5f33fb47c0abb6a3527b83bb66c8ce51e7e37
galang48 <galangyudha1945@gmail.com>
2024-03-14 11:24:58 +0700

    menambahkan prosedur display

--- InsectionSort/InsectionSort.cpp
+++ InsectionSort/InsectionSort.cpp
@@ -58,6 +58,18 @@ void insertionsort() {                  // Procedure Insertionsort
         }
     }
 }
+
+void display() {                                            // Procedure Display
+    cout << endl;                                           // Output baris kosong
+    cout << "\n===============================" << endl;    // Output ke layar
+    cout << "Element Array yang telah tersusun" << endl;    // Output ke layar
+    cout << "=================================" << endl;    // Output ke layar
+
+    for (int j = 0; j < n; j++) {                           // Looping dengan j dimulai dari 0 hingga n-1
+        cout << arr[j] << endl;                             // Output ke layar
+    }
+    cout << endl;                                           // Output baris kosong
+}
 int main()
 {
     std::cout << "Hello World!\n";
